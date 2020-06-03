4b2806fab6d0a02c73fdbeafe67172e0509b154f
taylorsweeft13 <taylorsweeft13@gmail.com>
2020-06-03 13:43:22 +0800

    bitwarden_update.sh

---
+++ bitwarden/bitwarden_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+docker stop bitwarden
+echo '1	stop bitwarden success'
+
+docker rm bitwarden
+echo '2	rm bitwarden success'
+
+docker pull bitwardenrs/server:latest
+echo '3	pull bitwarden success'
+
+docker run -d --name bitwarden\
+    --restart=always \
+    -v /file/bw-data/:/data/ \
+    -e ADMIN_TOKEN=0 \
+    -e SIGNUPS_ALLOWED=false \
+    -e WEBSOCKET_ENABLED=true \
+    -e LOG_FILE=/data/bitwarden.log \
+    -e DOMAIN=https://xxxxx\
+    -p 3080:80 \
+    -p 3012:3012 \
+bitwardenrs/server:latest
+echo '4	run bitwarden success'
